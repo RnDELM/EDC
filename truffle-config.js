90a3db6faeb74ee229abae140cb13dabe25b36df
RnDELM <innovationelm1@gmail.com>
2019-12-01 10:39:58 +0300

    Compiles assets for Github Pages

--- truffle-config.js
+++ truffle-config.js
@@ -28,6 +28,11 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: "8545",
+      network_id: "*" // match any network id
+    },
     rinkeby: {
       provider: function() {
         return new HDWalletProvider(mnemonic,
